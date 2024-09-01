44f1f8c0610699fffe42e8bd8d88c7fd076cd792
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-01 14:51:40 +0600

    added combine array

---
+++ combine_array.js
@@ -0,0 +1,12 @@
+// 5. Combining Arrays
+// Instructions:
+
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+let num1 = [1,2,3,4];
+let num2 = [6,7,8,10];
+console.log(num1,num2)
+let num3; 
+num3=num1.concat(num2);
+console.log(num3);
