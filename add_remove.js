6f67945891c0f0509444450ad7ca6b8fe44f4d0e
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-01 13:40:48 +0600

    added add_remove array elemebnts

---
+++ add_remove.js
@@ -0,0 +1,14 @@
+// 2. Add or remove elements
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+let spots = ['swizerland', 'thiland','usa'];
+console.log(spots);
+spots.push('shundarban');
+console.log(spots);
+spots.push('shalban','rangamati');
+console.log(spots);
+spots.pop();
+console.log(spots);
